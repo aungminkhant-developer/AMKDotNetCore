e703ba2be77f421036107400a51d7580ef003dc5
Aung Min Khant <aungminkhant.aln01@gmail.com>
2024-01-04 19:25:22 +0630

    [Complete] AMKDotNetCore.ATMWebApp

---
+++ AMKDotNetCore.ATMWebApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AMKDotNetCore.ATMWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AMKDotNetCore.ATMWebApp.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ AMKDotNetCore.ATMWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMKDotNetCore.ATMWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
